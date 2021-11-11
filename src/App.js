6f6aa4f1a709679b61021bc3afedb99de04563e0
Jessica MA <j.kalessa21@gmail.com>
2021-11-10 20:43:27 -0500

    modify app js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Fragment } from "react";
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Bienvenido a mi app Hardware Store</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <NavBar></NavBar>
+    </Fragment>
   );
 }
 
