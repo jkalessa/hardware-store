2c55747bad177110e26a26acb20dd6fb9ce6648f
Jessica MA <j.kalessa21@gmail.com>
2021-11-10 20:49:58 -0500

    add navbar js

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,17 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <nav className="navBar">
+      <ul>
+        <li>Inicio</li>
+        <li>Ofertas</li>
+        <li>Marcas</li>
+        <li>Nosotros</li>
+        <li>Contactanos</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
