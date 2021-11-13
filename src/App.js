2ade5897e80a70e6bedcb5a81be5a5ae2a57134f
Jessica MA <j.kalessa21@gmail.com>
2021-11-13 18:37:14 -0500

    add render itemlistcontainer in app js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import NavBar from "./components/navbar/NavBar";
+import ItemListContainer from "./components/itemListContainer/ItemListContainer";
 
 function App() {
   return (
     <Fragment>
-      <NavBar></NavBar>
+      <NavBar />
+      <ItemListContainer greetings="Bienvenido" />
     </Fragment>
   );
 }
