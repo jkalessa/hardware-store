412efd0cd44a6de5ca31e66fce0ed100a9e2b5ec
Jessica MA <j.kalessa21@gmail.com>
2021-11-22 20:09:22 -0500

    add component itemCount in App js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import NavBar from "./components/navbar/NavBar";
 import ItemListContainer from "./components/itemListContainer/ItemListContainer";
+import ItemCount from "./components/itemCount/ItemCount";
 
 function App() {
   return (
     <Fragment>
       <NavBar />
       <ItemListContainer greetings="Bienvenido" />
+      <ItemCount />
     </Fragment>
   );
 }
