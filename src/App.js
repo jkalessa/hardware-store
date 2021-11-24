f29eb307cf02419f0987f391b128023eab5b52cc
Jessica MA <j.kalessa21@gmail.com>
2021-11-24 16:53:57 -0500

    delete itemcount and props greetings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Fragment } from "react";
 import NavBar from "./components/navbar/NavBar";
 import ItemListContainer from "./components/itemListContainer/ItemListContainer";
-import ItemCount from "./components/itemCount/ItemCount";
 
 function App() {
   return (
     <Fragment>
       <NavBar />
-      <ItemListContainer greetings="Bienvenido" />
-      <ItemCount />
+      <ItemListContainer />
     </Fragment>
   );
 }
