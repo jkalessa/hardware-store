ad811c943694be255442f974e67bd58902681b5f
Jessica MA <j.kalessa21@gmail.com>
2021-12-01 17:25:12 -0500

    add fetch in itemlistcontainer

--- src/components/itemListContainer/ItemListContainer.js
+++ src/components/itemListContainer/ItemListContainer.js
@@ -2,12 +2,23 @@ import React from "react";
 import "./ItemListContainer.css";
 import ItemList from "../itemList/ItemList";
 import ItemCount from "../itemCount/ItemCount";
+import { useEffect, useState } from "react";
 
-const ItemListContainer = ({ greetings }) => {
+const ItemListContainer = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("https://cruce-app.herokuapp.com/products")
+        .then((response) => response.json())
+        .then((json) => setProducts(json));
+    }, 2000);
+  }, []);
+
   return (
     <div>
       <div className="contenedorSaludo">
-        <ItemList />
+        <ItemList datos={products} />
       </div>
       <div>
         <ItemCount stock={5} initial={1} />
