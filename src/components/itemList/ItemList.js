b788e466f8d82def5263d72557805df6eb0b96e0
Jessica MA <j.kalessa21@gmail.com>
2021-12-01 17:24:44 -0500

    remove fetch from itemlist

--- src/components/itemList/ItemList.js
+++ src/components/itemList/ItemList.js
@@ -1,21 +1,10 @@
-import React, { useEffect, useState } from "react";
 import "./ItemList.css";
 import Item from "../item/Item";
 
-const ItemList = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("https://cruce-app.herokuapp.com/products")
-        .then((response) => response.json())
-        .then((json) => setProducts(json));
-    }, 2000);
-  }, []);
-
+const ItemList = ({ datos }) => {
   return (
     <div className="contenedorItem">
-      {products.map((product) => {
+      {datos.map((product) => {
         return <Item data={product} />;
       })}
     </div>
