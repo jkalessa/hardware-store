87f8ea850e1c33e5a7311f4f6d01344005fb3b47
Jessica MA <j.kalessa21@gmail.com>
2021-12-02 23:14:19 -0500

    create component ButtonList

---
+++ src/components/buttonList/ButtonList.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button } from "semantic-ui-react";
+import "./ButtonList.css";
+
+const ButtonList = () => {
+  return <Button id="botonLista">Ver detalle del producto</Button>;
+};
+
+export default ButtonList;
