abd29bd6df09f6fc872c67cb27270f210bcd6504
Jessica MA <j.kalessa21@gmail.com>
2021-12-02 23:16:16 -0500

    add component function and import itemdetail

---
+++ src/components/itemDetailContainer/ItemDetailContainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./ItemDetailContainer.css";
+import ItemDetail from "../itemDetail/ItemDetail";
+
+const ItemDetailContainer = () => {
+  return <div>HOLA</div>;
+};
+
+export default ItemDetailContainer;
