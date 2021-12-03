6c9576622e1bc472e452a4da8505df3ad276c105
Jessica MA <j.kalessa21@gmail.com>
2021-12-02 23:14:58 -0500

    add styles buttom in item

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, Image } from "semantic-ui-react";
 import "./Item.css";
+import ButtonList from "../buttonList/ButtonList";
 
 const Item = ({ data }) => (
   <Card>
@@ -8,6 +9,9 @@ const Item = ({ data }) => (
     <Card.Content>
       <Card.Header>{data.name}</Card.Header>
       <Card.Description>{data.price}</Card.Description>
+      <div className="contenedorBoton">
+        <ButtonList></ButtonList>
+      </div>
     </Card.Content>
   </Card>
 );
